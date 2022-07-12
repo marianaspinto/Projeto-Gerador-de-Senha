40eb63ff16828442fd401dc9d5c6697625b993ca
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-07-12 18:55:55 -0300

    finalização do projeto com js

--- script.js
+++ script.js
@@ -7,5 +7,29 @@ let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
 let charset = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-let novasenha = "";
+let novaSenha = "";
 
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+    
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+      pass += charset.charAt(Math.floor(Math.random() * n));
+      
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass; 
+    novaSenha = pass;
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
